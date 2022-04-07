0a1bd6bd7b416487ff4df6520c86817995bd6ab7
krystal2022 <sjk5689612@gmail.com>
2022-04-07 21:12:14 +0900

    react practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+function Header(props){
+  return <header>
+    <h1><a href="/" onClick={function(event){
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
+  </header>
+}
+function Nav(props){
+  const lis = []
+  for (let i =0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a id={t.id} href={'/read/'+t.id} onClick={function(event){
+      event.preventDefault();
+      props.onChangeMode(Number(event.target.id));
+    }}>{t.title}</a></li>)
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
+}
+function Article(props){
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
 
 function App() {
+  const [mode, setMode] = useState('WELCOME');//기본값을 WELCOME으로 설정
+  const [id, setId] = useState(null);
+  const topics = [
+    {id:1, title:'html', body:'html is...'},
+    {id:2, title:'css', body:'css is...'},
+    {id:3, title:'js', body:'js is...'}
+  ]
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if(mode === 'READ'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      console.log(topics[i].id, id);
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  } else if(mode === 'CREATE'){
+    content = <Create></Create>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header title="WEB" onChangeMode={()=>{
+          setMode('WELCOME');
+        }}></Header>
+        <Nav topics={topics} onChangeMode={(_id)=>{
+          setMode('READ');
+          setId(_id);
+        }}></Nav>
+        {content}
+        <a href="/create" onClick={function(event){
+          event.preventDefault();
+          setMode('CREATE');
+        }}>Create</a>
     </div>
   );
 }
